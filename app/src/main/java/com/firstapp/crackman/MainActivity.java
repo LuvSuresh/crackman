3a879a34b068d94bd17d4c8eeb1ae2f4a78c0529
LuvSuresh <preetasuresh9@gmail.com>
2023-06-24 16:06:12 -0700

    Initial commit

---
+++ app/src/main/java/com/firstapp/crackman/MainActivity.java
@@ -0,0 +1,40 @@
+package com.firstapp.crackman;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.firstapp.crackman.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        o = new our_fragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragments,o).commit();
+    }
+}
+
+
+
+
+
+
+
+
+}
